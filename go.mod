09710cf82f5ae1ba3b7ff988f59733bab6aac510
Radek Dymacz <radekdymacz@gmail.com>
2020-03-08 21:00:02 +0100

    use errorgroup to fail on error

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/golang/protobuf v1.3.4
 	github.com/micro/go-micro/v2 v2.2.0
 	github.com/micro/protoc-gen-micro/v2 v2.0.0 // indirect
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
 )
--- handler/weather.go
+++ handler/weather.go
@@ -4,9 +4,10 @@ import (
 	"context"
 	"encoding/json"
 	"os"
-	"sync"
 	ow "weatherservice/openweather"
 	proto "weatherservice/proto"
+
+	"golang.org/x/sync/errgroup"
 )
 
 type Weather struct{}
@@ -14,26 +15,29 @@ type Weather struct{}
 func (w *Weather) Get(ctx context.Context, req *proto.Request, rsp *proto.Response) error {
 	//rsp.Response = req.Cities
 	client := ow.OpenWeatherMap{API_KEY: os.Getenv("OWM_API_KEY")}
-	var wg sync.WaitGroup
-	wg.Add(len(req.Cities))
+	var g errgroup.Group
+
 	var response []*proto.CurrentWeatherResponse
-	for i := 0; i < len(req.Cities); i++ {
-		go func(city string) {
-			defer wg.Done()
-			currentWeather, _ := client.CurrentWeatherFromCity(city)
-			// if err != nil {
-			// 	return err
-			// }
+	for _, city := range req.Cities {
+		g.Go(func() error {
+			currentWeather, err := client.CurrentWeatherFromCity(city)
+			if err != nil {
+				return err
+			}
 			var cwr proto.CurrentWeatherResponse
 			// unmarshal the byte stream into a Go data type
-			_ = json.Unmarshal(currentWeather, &cwr)
-			// if jsonErr != nil {
-			// 	return jsonErr
-			// }
+			jsonErr := json.Unmarshal(currentWeather, &cwr)
+			if jsonErr != nil {
+				return jsonErr
+			}
 			response = append(response, &cwr)
-		}(req.Cities[i])
+			return nil
+		})
 	}
-	wg.Wait()
+	if err := g.Wait(); err != nil {
+		return err
+	}
+
 	rsp.Response = response
 	return nil
 }
